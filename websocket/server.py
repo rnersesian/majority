fa17d775df2e231aaf21f154702c78770fa5dc84
NERSESIAN <robert.nersesian@sogeti.com>
2024-04-15 15:44:29 +0200

    manage player connection to websocket

--- websocket/server.py
+++ websocket/server.py
@@ -1,15 +1,19 @@
 import websockets
 import datetime
-import random
+import json
 import asyncio
 
 
 CONNECTIONS = set()
 
+PLAYERS = []
+ROOMS = []
 
 async def register(websocket):
 
     if websocket not in CONNECTIONS:
+        greetings = await websocket.recv()
+        print(f">>> {greetings}")
         CONNECTIONS.add(websocket)
         print(f'Number of connection: {len(CONNECTIONS)}')
     else:
@@ -19,11 +23,33 @@ async def register(websocket):
     try:
         await websocket.wait_closed()
     finally:
-        print("Someone disconnected")
         CONNECTIONS.remove(websocket)
 
-    
 
+async def register_player(websocket):
+    global PLAYERS, CONNECTIONS, ROOMS
+
+    if websocket not in CONNECTIONS:
+        player = await websocket.recv()
+        player = json.loads(player)
+        player["websocket"] = websocket
+        PLAYERS.append(player)
+    else:
+        return
+    
+    for pl in PLAYERS:
+        print(pl)
+    
+    try:
+        await websocket.wait_closed()
+    finally:
+        PLAYERS = filter(lambda pl: pl["websocket"] == websocket, PLAYERS)
+        CONNECTIONS.remove(websocket)
+        
+    
+async def handle_quizz():
+    global ROOMS, PLAYERS
+    
 
 async def show_time():
     while True:
@@ -33,7 +59,7 @@ async def show_time():
 
 
 async def main():
-    async with websockets.serve(register, "localhost", 8001):
+    async with websockets.serve(register_player, "localhost", 8001):
         await show_time()
 
 
