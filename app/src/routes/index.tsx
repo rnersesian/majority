16512df7a9103f4fc0f72319ec5eeb83fd185733
Kasmir <nersesian.robert@gmail.com>
2024-05-15 14:58:00 +0000

    connect to websocket from front

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,18 +1,46 @@
 import { Title } from "@solidjs/meta";
-import { Show, createEffect, createSignal } from "solid-js";
+import { Show, createEffect, createSignal, onMount } from "solid-js";
 import Cookie from "js-cookie"
 import { Navigate, useNavigate } from "@solidjs/router";
+import { WsEvents, sendRequests } from "~/utils/WSEvent";
 
 export default function Home() {
 
   const [username, setUsername] = createSignal("")
   const [joined, setJoined] = createSignal(false)
+  const [socket, setSocket] = createSignal({} as WebSocket)
+
   
-  const on_connect = () => {
-    let _username = (document.getElementById("username") as HTMLInputElement).value
-    Cookie.set("username", _username)
-    setJoined(true)
+  
+  onMount(async () => {
+    setSocket(new WebSocket("ws://77.37.86.225:8001"))
+    console.log("hey")
+
+    socket().addEventListener("message", (ws_event) => {
+      let event = JSON.parse(ws_event.data)
+      
+      // Filtering events recieved by websocket 
+      switch (event.type) { 
+        case WsEvents.CONNECT_SUCCESS:
+          let _username = (document.getElementById("username") as HTMLInputElement).value
+          Cookie.set("username", _username)
+          setJoined(true)
+          break;
+
+        default:
+          console.log("Whut ?");
+          break;
+      }
+    })
+  })
+
+  // When User enter its Username
+  const on_connect = async () => {
+    if (username() !== "") {
+      await sendRequests(socket(), WsEvents.CONNECT, {player_name: username()})
+    }
   }
+
   const navigate = useNavigate()
 
   createEffect(() => {
@@ -24,7 +52,7 @@ export default function Home() {
   // Login form that appears first
   const login_form =
     <div style={{margin: "auto", "margin-top": "100px"}}>
-      <form>
+      <div>
         <div class="form-input">
           <label for="username">Username</label> <br/>
           <input type="text" id="username" placeholder="John Doe" value={username()} onChange={(event) => setUsername(event.currentTarget.value)}/>
@@ -32,7 +60,7 @@ export default function Home() {
         <div class="form-input">
           <button value="Connect" class="submit" onClick={on_connect}>Connect</button>
         </div>
-      </form> 
+      </div> 
     </div>
 
 
---
+++ app/src/utils/WSEvent.tsx
@@ -0,0 +1,26 @@
+export async function sendRequests(websocket: WebSocket, event_type: string, data: object)
+{
+    websocket.send(JSON.stringify({
+        "type": event_type,
+        "data" : data
+    }))
+}
+
+
+export let WsEvents = {
+    CONNECT             : "connect",
+    CONNECT_SUCCESS     : "connect_success",
+    MESSAGE             : "message",
+    SHOW_ROOMS          : "show_rooms",
+    CREATE_ROOM         : "create_room",
+    DELETE_ROOM         : "delete_room",
+    JOIN_ROOM           : "join_room",
+    SEND_QUESTIONS      : "send_questions",
+    SEND_ANSWER         : "send_answer",
+    SEND_SCORE          : "send_score",
+    ERROR               : "error",
+    CHAT_MESSAGE        : "chat_message",
+    REFRESH_PLAYER_LIST : "refresh_player_list"
+}
+
+
