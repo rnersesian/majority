3f5139d23e1a51a1dd61594b9b82f88411dab841
Kasmir <nersesian.robert@gmail.com>
2024-05-15 13:03:58 +0000

    front interface to connect

--- app/src/routes/about.tsx
+++ app/src/routes/about.tsx
@@ -1,6 +1,10 @@
 import { Title } from "@solidjs/meta";
+import { useLocation } from "@solidjs/router";
 
 export default function Home() {
+
+  const location = useLocation()
+
   
 
   return (
--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,34 +1,59 @@
 import { Title } from "@solidjs/meta";
-import { Show, createSignal } from "solid-js";
+import { Show, createEffect, createSignal } from "solid-js";
+import Cookie from "js-cookie"
+import { Navigate, useNavigate } from "@solidjs/router";
 
 export default function Home() {
-  const [username, setUsername] = createSignal("")
 
+  const [username, setUsername] = createSignal("")
+  const [joined, setJoined] = createSignal(false)
+  
   const on_connect = () => {
     let _username = (document.getElementById("username") as HTMLInputElement).value
-    setUsername(_username)
-    console.log(username())
+    Cookie.set("username", _username)
+    setJoined(true)
   }
+  const navigate = useNavigate()
 
-  console.log(username())
+  createEffect(() => {
+    setUsername(Cookie.get('username') || "")
+  })
 
 
-    const login_form = <main style={{margin: "auto", "margin-top": "100px"}}>
-      <Title>Home</Title>
+  
+  // Login form that appears first
+  const login_form =
+    <div style={{margin: "auto", "margin-top": "100px"}}>
       <form>
         <div class="form-input">
           <label for="username">Username</label> <br/>
-          <input type="text" id="username" placeholder="John Doe"/>
+          <input type="text" id="username" placeholder="John Doe" value={username()} onChange={(event) => setUsername(event.currentTarget.value)}/>
         </div>
         <div class="form-input">
           <button value="Connect" class="submit" onClick={on_connect}>Connect</button>
         </div>
       </form> 
-    </main>
+    </div>
+
 
   return (
-    <Show when={(username() !== "")} fallback={login_form}>
-      <div>Hey {username()}</div>
-    </Show>
+    <main>
+      <Title>Home</Title>
+      <div class="content_center">
+        <div></div>
+          <Show when={joined()} fallback={login_form}>
+            <div class="tspacing-x">
+              <form>
+                <div class="inline-input">
+                  <label for="room_name">Create Room</label>
+                  <input id="room_name" placeholder="Room Name"/>
+                  <button class="submit">Create</button>
+                </div>
+              </form> 
+            </div>
+          </Show>
+        <div></div>
+      </div>
+    </main>
   )
 }
